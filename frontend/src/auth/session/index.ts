96142a80e9b71bd499f4bf111878afb12024bb4e
rarescovei5 <rarescovei5@gmail.com>
2025-01-11 18:40:12 +0200

    Deleted Auth File And Created API File

--- frontend/src/auth/session/index.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const Signup = async () => {
-  const response = await axios.post('/api/auth/signup');
-  return response.data;
-};
-export const Authenticate = async () => {};
