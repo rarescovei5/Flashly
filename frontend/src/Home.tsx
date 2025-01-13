66f1e1d8cd55bef5c8e14b3002c7935cbc67e16b
rarescovei5 <rarescovei5@gmail.com>
2025-01-13 23:21:46 +0200

    Created Navbar Component

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -2,6 +2,7 @@ import useAuth from './hooks/useAuth';
 import { Link } from 'react-router-dom';
 import useRefreshToken from './hooks/useRefreshToken';
 import { useEffect } from 'react';
+import Navbar from './components/Navbar';
 
 const Home = () => {
   const { auth } = useAuth();
@@ -13,19 +14,7 @@ const Home = () => {
 
   return (
     <div className="flex flex-col gap-4">
-      <Link to={'/sign-in'}>Sign In</Link>
-      <Link to={'/sign-up'}>Sign Up</Link>
-      <Link to={'/decks'}>decks</Link>
-      <Link to={'/decks/discover'}>discover</Link>
-      <br />
-      <br />
-      <br />
-      <button onClick={() => refresh()}>Ceva</button>
-      <br />
-      <br />
-      <h1 className="h1">
-        {auth?.email ? `Welcome ${auth.email}` : 'You are not logged in'}
-      </h1>
+      <Navbar />
     </div>
   );
 };
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,51 @@
+import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { auth } = useAuth();
+
+  return (
+    <div className="flex items-center w-[80%] mx-auto mt-4">
+      <div className="flex-1">
+        <h3 className="h3">Flashly</h3>
+      </div>
+      <div className="rounded-2xl bg-c-light flex-1 px-4 py-2 flex">
+        <img className=" mr-2" src="/search.svg" alt="" />
+        <input
+          className="flex-1 p-small text-[#C6C6C6] placeholder:p-small placeholder:text-[#C6C6C6] bg-transparent   rounded-2xl outline-none"
+          type="text"
+          placeholder="Search flash cards"
+        />
+      </div>
+      <div className="flex-1 flex justify-end">
+        {auth?.email ? (
+          <div className="inline-flex gap-4 items-center ">
+            <Link className="p-small" to={'/decks'}>
+              Decks
+            </Link>
+            <button className="bg-c-primary p-2 rounded-full">
+              <img src="/man.svg" alt="" />
+            </button>
+          </div>
+        ) : (
+          <div className="inline-flex gap-4 ">
+            <Link
+              className="p-small bg-c-light px-4 py-2 rounded-2xl"
+              to="/sign-in"
+            >
+              Signin
+            </Link>
+            <Link
+              className="p-small bg-c-primary text-c-dark px-4 py-2 rounded-2xl"
+              to="/sign-up"
+            >
+              Signup
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
