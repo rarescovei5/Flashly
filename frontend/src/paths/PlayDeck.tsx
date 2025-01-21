49ed442e847308b9ea4b5349d6ab6c3582885d29
rarescovei5 <rarescovei5@gmail.com>
2025-01-21 23:58:44 +0200

    bug

--- frontend/src/paths/PlayDeck.tsx
+++ frontend/src/paths/PlayDeck.tsx
@@ -3,8 +3,8 @@ import Navbar from '../components/Navbar';
 import useDeck from '../hooks/useDeck';
 
 const PlayDeck = () => {
-  const deckId = useParams().deckId;
-  const { cards } = useDeck(deckId!);
+  const deckId = parseInt(useParams().deckId!);
+  const { cards } = useDeck(deckId);
 
   return (
     <>
