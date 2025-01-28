45a443c74294f8c4612c6e11b6201c849c54abc3
rarescovei5 <rarescovei5@gmail.com>
2025-01-28 21:51:16 +0200

    added timeout

--- backend/index.ts
+++ backend/index.ts
@@ -22,6 +22,7 @@ const mysqlConnection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  connectTimeout: 20000,
 });
 
 function connectToDatabase() {
